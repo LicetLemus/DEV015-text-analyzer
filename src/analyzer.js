4cf92abfde664b65844b47155b57520931190d98
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-05-22 15:37:13 -0500

    Funcionalidad getNumberCount/ class en html(li)

--- src/analyzer.js
+++ src/analyzer.js
@@ -5,9 +5,12 @@ const analyzer = {
       return 0;
     }
 
-    const words = text.trim().split(/\s+/).filter(function (word) {
-      return word.length > 0;
-    });
+    const words = text
+      .trim()
+      .split(/\s+/)
+      .filter(function (word) {
+        return word.length > 0;
+      });
 
     return words.length;
   },
@@ -17,20 +20,43 @@ const analyzer = {
     return text.length;
   },
 
-  // 2. getCharacterCountExcludingSpaces
+  // 3. getCharacterCountExcludingSpaces
 
   getCharacterCountExcludingSpaces: (text) => {
-
     // sin g de global, only replace the first coincidence
     // replace(patron, replace)
-    const characterWithoutSpace = text.replace(/[\s.,/#!$%^&*;:{}=\-_~()]/g, "");
+    const characterWithoutSpace = text.replace(
+      /[\s.,/#!$%^&*;:{}=\-_~()]/g,
+      ""
+    );
     return characterWithoutSpace.length;
+  },
+
+  // 4. getNumberCount
+  getNumberCount: (text) => {
+    const words = text.trim().split(/\s+/).filter(function (word) {
+      return word.length > 0;
+    });
+
+    const removeLastDot = (number) => {
+      return number.replace(/\.$/, "")
+    } 
+
+    let count = 0;
+    for (let i = 0; i < words.length; i++) {
+      const number = Number(removeLastDot(words[i]))
+      if (number && words[i] !== " ") {
+        count++;
+      }
+    }
+    return count
   },
 
   /*
 getAverageWordLength: (text) => {    
-  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
 },
+
 getNumberCount: (text) => {
   //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
 },
--- src/index.js
+++ src/index.js
@@ -7,17 +7,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const wordCountShow = document.querySelector(".word-count");
   const characterCountShow = document.querySelector(".count-character");
   const characterWithoutSpaceShow = document.querySelector(".character-without-space")
+  const getNumberCountShow = document.querySelector(".number")
 
   textarea.addEventListener("input", function () {
     const text = textarea.value;
     const wordCount = analyzer.getWordCount(text);
     const characterCount = analyzer.getCharacterCount(text);
     const characterWithoutSpace = analyzer.getCharacterCountExcludingSpaces(text);
+    const numberCount = analyzer.getNumberCount(text)
 
     // actualiza el contenido del elemento con la clase .word-count almacenado en wordCountShow en la página web
     wordCountShow.textContent = `Palabras: ${wordCount}`;
     characterCountShow.textContent = `Caracter: ${characterCount}`;
-    characterWithoutSpaceShow.textContent = `Caracteres sin espacio: ${characterWithoutSpace} `
+    characterWithoutSpaceShow.textContent = `Caracteres sin espacio: ${characterWithoutSpace}`
+    getNumberCountShow.textContent = `Números: ${numberCount}`
   });
 });
 
